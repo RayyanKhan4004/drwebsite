2bf043b149f24c757d53ff416b84f1fa580b96be
Rayyan Khan <160082300+RayyanKhan4004@users.noreply.github.com>
2024-09-30 14:02:22 -0700

    Update HomeServics.jsx

--- src/routes/HomeServics.jsx
+++ src/routes/HomeServics.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
 import SearchBar from '../components/SearchBar'
+import Card1 from '../components/Card1'
 import LocatiionHomeHealth from '../components/LocatiionHomeHealth'
 import LocationServicesWeProvideCards from '../components/LocationServicesWeProvideCards'
 import WhatsAppFloatingIcon from '../components/WhatsAppFloatingIcon'
@@ -12,7 +13,8 @@ function Location() {
       <WhatsAppFloatingIcon phoneNumber={'03244574564'} message={`I there I have visited your website I'm interested in getting more information about it`} />
   <div className='p-6' >
         <LocatiionHomeHealth/>
-        <LocationServicesWeProvideCards/>         
+        <LocationServicesWeProvideCards/>   
+        <Card1/>      
 
       <SearchBar/>
   </div>
