99f7599fbe280ca12047228ea24319da1ea694d2
Rayyan Khan <160082300+RayyanKhan4004@users.noreply.github.com>
2024-10-04 17:56:53 +0500

    Update Location.jsx

--- src/routes/Location.jsx
+++ src/routes/Location.jsx
@@ -11,18 +11,18 @@ function Location() {
   return (
   <>
       <Navbar/>
-      <WhatsAppFloatingIcon phoneNumber={'+923104168104'} message={`I there I have visited your website I'm interested in getting more information about it`} />
+      <WhatsAppFloatingIcon phoneNumber={'+923104168104'} message={`Hi there I have visited your website I'm interested in getting more information about it`} />
   <div className='p-6' >
       <div className='flex justify-evenly items-center' >
 
 
 
-    <div className='w-auto  flex items-center justify-center ' >
+    <div className='max-sm:w-auto  flex items-center justify-center ' >
    
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.194928259115!2d74.19000967530496!3d31.43630015129128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3918fe6b04621a15%3A0x3cb9645afd02f344!2sSaleem%20Memorial%20Hospital!5e0!3m2!1sen!2s!4v1726776625340!5m2!1sen!2s" width="auto" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
     </div>
-      <div className='w-auto  flex items-center justify-center '  >
+      <div className='max-sm:w-auto  flex items-center justify-center '  >
       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13612.576833699277!2d74.2358843!3d31.465219!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919021ea668a7e3%3A0xe50da7d1f9af0846!2sFarooq%20Hospital%20West%20Wood%20Branch%20Lahore!5e0!3m2!1sen!2s!4v1727729227214!5m2!1sen!2s"width="auto" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
       </div>
